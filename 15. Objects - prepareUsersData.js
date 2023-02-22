8257a87ead07228851a28789e35c19947a7d7aff
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2023-02-22 18:01:36 +0200

    Create 15. Objects - prepareUsersData.js

---
+++ 15. Objects - prepareUsersData.js
@@ -0,0 +1,46 @@
+const userList = [{
+    firstName: 'Bob',
+    lastName: 'Marley',
+    age: 21,
+    balance: '12.2$'
+},
+{
+    firstName: 'Ariana',
+    lastName: 'Grande',
+    age: 17,
+    balance: '162$'
+},
+{
+    firstName: 'Bill',
+    lastName: 'Gates',
+    age: 18,
+    balance: '15.4$'
+},
+{
+    firstName: 'John',
+    lastName: 'Bonjovi',
+    age: 19,
+    balance: '-12.0$'
+}
+];
+
+
+function preparedData(users) {
+    const mappedUsers = users.filter(item => item.age >= 18).map(x => {
+        return {
+            age: x.age,
+            name: x.firstName + ' ' + x.lastName,
+            balance: x.balance
+        };
+    });
+    const balanceSum = mappedUsers.reduce((sum, item) => {
+        return sum += +item.balance.replace('$', '');
+    }, 0);
+    mappedUsers.push({ userBalanceSum: parseFloat(balanceSum).toFixed(2) + '$' });
+    console.log(mappedUsers);
+
+    const userNegativeBalance = mappedUsers.find(user => +user.balance.replace('$', '') < 0);
+    console.log(userNegativeBalance);
+}
+
+preparedData(userList); 
