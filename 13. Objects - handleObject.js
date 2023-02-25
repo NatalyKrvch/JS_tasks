cabfee99e6e2d55204a340b44f0ee4f32acf6cde
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2023-02-26 00:39:15 +0200

    Update 13. Objects - handleObject.js

--- 13. Objects - handleObject.js
+++ 13. Objects - handleObject.js
@@ -1,3 +1,17 @@
+// Task description:
+
+// You need to create a function that will work with objects.
+// The function takes 3 parameters:
+// 1. obj. The object with which the function will work
+// 2. key. object key
+// 3. action. The action we will take over the object
+// The action parameter can be 4 values:
+// 1. 'get'. If the action is 'get', the function should return the key value in the obj object.
+// 2. 'add'. If the action is 'add', the function must add a new key to the object and assign an empty string "". 
+//     The updated obj must also be returned from the function.
+// 3. 'delete'. If the action is 'delete', the function should remove the key property from the object and return the updated object. 
+// If the action is equal to any other value, the function must return obj.
+
 const handleObject = (obj, key, action = 'get') => {
     switch (action) {
         case 'get':
