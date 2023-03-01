de4b514db02463499988b2320987504280c83cb6
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2023-03-01 16:21:04 +0200

    Update 17. Date - getDaysBeforeBirthday.js

--- 17. Date - getDaysBeforeBirthday.js
+++ 17. Date - getDaysBeforeBirthday.js
@@ -1,3 +1,9 @@
+// Task description:
+
+// Your task now is to create a function that will return the number of days until the next birthday. 
+// This function should accept 1 parameter, which is a date object and indicates the date of your nearest birthday. 
+// The year should be the closest to the current date, not the year of your birth.
+
 const convertMsToDays = (ms) => Math.round(ms / 1000 / 60 / 60 / 24);
 
 const getDaysBeforeBirthday = (birthdayDate) => {
